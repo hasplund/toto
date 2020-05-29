eb4f9238f90822408f6d917b910094f7b33aeaf5
Henrik J. Asplund <henrik.asplund@iki.fi>
2020-05-30 00:03:20 +0300

    Card loading done

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -1,15 +1,47 @@
 const Agenda = require('agenda');
+const axios = require('axios');
+
+const Card = require('../model/card')
 
 // Or override the default collection name:
-const agenda = new Agenda({db: {address: mongoConnectionString, collection: 'agendaJobs'}});
+const agenda = new Agenda({db: {address: "mongodb://localhost:27017/toto", collection: 'agenda'}});
 
-/*
-agenda.define('delete old users', async job => {
-    await User.remove({lastLogIn: {$lt: twoDaysAgo}});
+function prepareCardData(card) {
+    let keys = ['cardId', 'country', 'firstRaceStart', 'meetDate',
+        'trackAbbreviation', 'trackName', 'raceType']
+    let prepareData = {};
+    for (let key of keys) {
+        prepareData[key]=card[key];
+    }
+    return prepareData;
+}
+
+agenda.define('load cards', async job => {
+    console.log("load cards");
+    axios({
+        method: 'get',
+        url: 'https://www.veikkaus.fi//api/toto-info/v1/cards/active',
+        responseType: 'json'
+    })
+        .then(function (response) {
+            let coll = response['data']['collection']
+            for (let card of coll) {
+                let data = prepareCardData(card)
+                let cardId=data['cardId']
+                Card
+                    .findOneAndUpdate({cardId: cardId},
+                        {$set: data},
+                        {new:true, upsert:true, useFindAndModify:true})
+                    .exec(function (err, myCard) {
+                        if (err) throw err;
+                        console.log(myCard);
+                    })
+            }
+        })
 });
 
-(async function() { // IIFE to give access to async/await
+(async function () { // IIFE to give access to async/await
     await agenda.start();
-
-    await agenda.every('3 minutes', 'delete old users');
-*/
+    await agenda.purge();
+    await agenda.every('10 minutes', 'load cards');
+})();
---
+++ start-agendash.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+npx agendash --db=mongodb://localhost/toto --collection=agenda --port=3001
